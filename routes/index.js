b24ab2adbca3b85e5096647e559bb7b3ccbb55be
Juan Castillo <Juanpablodlcc@gmail.com>
2023-06-15 14:03:53 -0600

    [FIX] vercel

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+  res.send({ message: 'Hello world' });
+});
+
 router.use("/users", require("./users"))
 
 module.exports = router;
